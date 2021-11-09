855840f22d7a533c3774d7d8f550e222eaccf180
John K <john.kellyfitness@gmail.com>
2021-11-09 10:24:59 -0500

    fixing db

--- connect.js
+++ connect.js
@@ -3,8 +3,8 @@ const mysql = require('mysql2')
 
 const db = mysql.createConnection
 ({
-  host: 'process.env.aaoixndxpezyoj.chqvmqyjerdw.us-east-2.rds.amazonaws.com',
-  user: 'process.env.root',
+  host: 'process.env.adatabase-1.chqvmqyjerdw.us-east-2.rds.amazonaws.com',
+  user: 'process.env.admin',
   password: 'process.env.Johnisbroke!1',
   port     : 'process.env.3306',
   database: "joblog"
